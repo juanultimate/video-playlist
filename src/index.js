6fba926b68ef20e334ab1a8d2aa4c7d2e3232c88
Juan Gabriel Guzman <jguzman@ioet.com>
2018-04-08 23:54:58 -0500

    Hola mundo con react

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const app = document.getElementById("app");
+const holaMundo = <h3> Hola Edmundo!  </h3>
+
+ReactDOM.render(holaMundo, app);
+
+// console.log('hola mundo');
